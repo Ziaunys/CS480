2654db7cdf16d378269e2c92893e007361c7d4b0
Eric Zounes <zounes99@gmail.com>
2012-02-06 21:51:05 -0800

    All tokens matching except T_NEQ

--- ms2_scanner.rb
+++ ms2_scanner.rb
@@ -14,8 +14,14 @@ end
 
 def tokenize(new_t)
 
-new_t.to_s
-
+new_t.gsub!(/\d+\.\d+/,'T_FDIG')
+if new_t == 'T_FDIG'
+return new_t
+end
+new_t.sub!(/\bfloat\b/,'T_FLOAT')
+if new_t == 'T_FLOAT'
+return new_t
+end
 new_t.sub!(/\b\d+\b/,'T_IDIG')
 if new_t == 'T_IDIG'
 return new_t
@@ -28,27 +34,27 @@ new_t.sub!(/\bwhile\b/,'T_WHL')
 if new_t == 'T_WHL'
 return new_t
 end
-new_t.sub!(/\b<\b/,'T_LT')
+new_t.sub!('<','T_LT')
 if new_t == 'T_LT'
 return new_t
 end
-new_t.sub!(/\b>\b/,'T_GT')
+new_t.sub!('>','T_GT')
 if new_t == 'T_GT'
 return new_t
 end
-new_t.sub!(/\b<=\b/,'T_LTEQ')
+new_t.sub!('<=','T_LTEQ')
 if new_t == 'T_LTEQ'
 return new_t
 end
-new_t.sub!(/\b>=\b/,'T_GTEQ')
+new_t.sub!('>=','T_GTEQ')
 if new_t == 'T_GTEQ'
 return new_t
 end
-new_t.sub!(/\b==\b/,'T_EQ')
+new_t.sub!('==','T_EQ')
 if new_t == 'T_EQ'
 return new_t
 end
-new_t.sub!(/\b=\b/,'T_ASS')
+new_t.sub!('=','T_ASS')
 if new_t == 'T_ASS'
 return new_t
 end
@@ -56,11 +62,19 @@ new_t.sub!('!=','T_NEQ')
 if new_t == 'T_NEQ'
 return new_t
 end
-new_t.sub!(/\b\(\b/,'T_LPAR')
+new_t.sub!('&&','T_AND')
+if new_t == 'T_AND'
+return new_t
+end
+new_t.sub!('||','T_OR')
+if new_t == 'T_OR'
+return new_t
+end
+new_t.sub!('(','T_LPAR')
 if new_t == 'T_LPAR'
 return new_t
 end
-new_t.sub!(/\b\)\b/,'T_RPAR')
+new_t.sub!(')','T_RPAR')
 if new_t == 'T_RPAR'
 return new_t
 end
@@ -68,19 +82,19 @@ new_t.sub!('+','T_ADD')
 if new_t == 'T_ADD'
 return new_t
 end
-new_t.sub!(/\b\-\b/,'T_SUB')
+new_t.sub!('-','T_SUB')
 if new_t == 'T_SUB'
 return new_t
 end
-new_t.sub!(/\b\*\b/,'T_MUL')
+new_t.sub!('*','T_MUL')
 if new_t == 'T_MUL'
 return new_t
 end
-new_t.sub!(/\b\\\b/,'T_DIV')
+new_t.sub!('/','T_DIV')
 if new_t == 'T_DIV'
 return new_t
 end
-new_t.sub!(/\b%\b/,'T_MOD')
+new_t.sub!('%','T_MOD')
 if new_t == 'T_MOD'
 return new_t
 end
@@ -108,24 +122,16 @@ new_t.sub!(/\bint\b/,'T_INT')
 if new_t == 'T_INT'
 return new_t
 end
-new_t.sub!(/\bfloat\b/,'T_FLOAT')
-if new_t == 'T_FLOAT'
-return new_t
-end
 new_t.sub!(/\b[a-zA-Z]\w*\b/,'T_ID')
 if new_t == 'T_ID'
 return new_t
 end
-new_t.sub!(/\b\d+\.\d+\b/,'T_FDIG')
-if new_t == 'T_FDIG'
-return new_t
-end
-
 return nil
 end
 def lexer(s_file)
 	text = File.open(s_file.to_s, "r").read
-	output = ((text.split(/( >=|<=|==|[+\-\/*%\(\)=]|!=)/).join(' ')).split(/\s+/))
+	puts text.split
+	output = ((text.split(/(>=|<=|==|[\+\-\/\*%\(\)=]|!=)/).join(' ')).split(/\s+/))
 	puts output
 	output.each { |i| tokenize(i) }
 	
